8eef430b190ff2deb3619944c2452cce479e0f88
Kirill <kiril-shitikov@yandex.ru>
2023-04-16 20:11:54 +0300

    docs: Семинар: задача №1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №1
+// Напишите программу, которая принимает на вход координаты точки (X и Y), причем x не равен 0 и Y не равен 0 и выдает номер четверти плоскости в которой находится эта точка.
+
+// Console.WriteLine("Введите число");
+// int X = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int Y = Convert.ToInt32(Console.ReadLine());
+
+// if ((X > 0) && (Y > 0))
+// {
+//     Console.WriteLine("№1");
+// }
+
+// else if ((X < 0) && (Y > 0))
+// {
+//     Console.WriteLine("№2");
+// }
+
+// else if ((X < 0) && (Y < 0))
+// {
+//     Console.WriteLine("№3");
+// }
+
+// else if ((X > 0) && (Y < 0))
+// {
+//     Console.WriteLine("№4");
+// }
